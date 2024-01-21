b24b6d7974015bfa13ed3970fc759160a8ae38b0
L1BER2Y <xerdan.9@mail.ru>
2024-01-22 00:00:45 +0300

    added web module

---
+++ web/src/main/java/by/shershen/web/WebApplication.java
@@ -0,0 +1,12 @@
+package by.shershen.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"by.shershen.service", "by.shershen.database"})
+public class WebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
+}
---
+++ web/src/main/java/by/shershen/web/controller/UserController.java
@@ -0,0 +1,36 @@
+package by.shershen.web.controller;
+
+import by.shershen.service.api.IUserService;
+import by.shershen.service.dto.PageDTO;
+import by.shershen.service.dto.UserDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/user")
+public class UserController {
+    private final IUserService userService;
+
+    public UserController(IUserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(value = "/add")
+    @ResponseBody
+    public ResponseEntity<String> add(@RequestBody UserDTO userDTO
+    ) {
+        this.userService.save(userDTO);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping(value ="/find")
+    @ResponseBody
+    public Page<UserDTO> findAll(@RequestParam(value = "page", defaultValue = "0") Integer page,
+                                 @RequestParam(value = "size", defaultValue = "10") Integer size
+    ) {
+        PageDTO pageDTO = new PageDTO(page, size);
+        return this.userService.findAll(pageDTO);
+    }
+}
